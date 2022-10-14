46e4469420f96cebaf423a93aac9c4e3442ab658
wangshuo <xujieiata@163.com>
2022-10-14 11:30:28 +0800

    测试用例

---
+++ spider/src/utils/utils.go
@@ -0,0 +1,12 @@
+package utils
+
+import "math/rand"
+
+func RandomString() string {
+	letterBytes := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	b := make([]byte, rand.Intn(10)+10)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
+}
