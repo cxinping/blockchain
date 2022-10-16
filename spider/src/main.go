e778aee8fde73aaccbd54093b48d3f708e84e40e
wangshuo <xujieiata@163.com>
2022-10-16 19:34:26 +0800

    例子11

--- spider/src/main.go
+++ spider/src/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"spider/src/config"
 	"spider/src/crawl"
+	"spider/src/task"
 )
 
 func main() {
 	db := config.InitDB() // 初始化数据库
 	defer db.Close()
 
-	crawl.CrawlMatches()
+	crawl.CrawlMatches() //爬取赛事和比赛数据
+	task.QueryMatches()  //执行调度任务
 }
--- spider/src/task/tasks.go
+++ spider/src/task/tasks.go
@@ -1,1 +1,27 @@
 package task
+
+import (
+	"fmt"
+	"github.com/robfig/cron/v3"
+	"time"
+)
+
+type MyJob func()
+
+func (f MyJob) Run() {
+	currentTime := time.Now().Format("2006-01-02 15:04:05")
+	fmt.Println("* myJob 查询比赛列表修改状态 ", currentTime)
+}
+
+var Cron = cron.New(cron.WithSeconds())
+
+func QueryMatches() {
+	// 查询比赛列表修改状态
+	fmt.Println("* 查询比赛列表修改状态")
+	Cron.AddJob("@every 5s", MyJob(func() {
+		fmt.Println("myjob")
+	}))
+
+	Cron.Start()
+	select {} //阻塞主线程停止
+}
