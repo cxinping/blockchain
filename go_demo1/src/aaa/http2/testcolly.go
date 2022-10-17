24adbd4f910b34d7761d3d53e103f22504584cfa
wangshuo <xujieiata@163.com>
2022-10-17 15:40:16 +0800

    例子10

--- go_demo1/src/aaa/http2/testcolly.go
+++ go_demo1/src/aaa/http2/testcolly.go
@@ -31,11 +31,51 @@ func Example2() {
 	// selector goquery name id class
 	c.OnHTML(".sidebar-link", func(e *colly.HTMLElement) {
 		//e.Request.Visit(e.Attr("href"))
-		link := e.Attr("href")
-		link = "https://gorm.io/zh_CN/docs/" + link
-		fmt.Printf("Link found: %q -> %s\n", e.Text, "https://gorm.io/zh_CN/docs/"+link)
+		fmt.Println("OnResponse收到html内容后调用:OnHTML ")
 
-		c.Visit(e.Request.AbsoluteURL(link))
+		link := e.Attr("href")
+		link = e.Request.AbsoluteURL(link)
+
+		fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+		fmt.Println("")
+		//c.Visit(e.Request.AbsoluteURL(link))
+	})
+
+	c.OnResponse(func(r *colly.Response) {
+		fmt.Println("获得响应后调用:OnResponse")
+		//fmt.Println("Visited", r.Request.URL)
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("访问网页 ", r.URL.String())
+	})
+
+	c.Visit("https://gorm.io/zh_CN/docs")
+}
+
+func Example3() {
+	c := colly.NewCollector()
+
+	// selector goquery name id class
+	c.OnHTML(".sidebar-link", func(e *colly.HTMLElement) {
+		//e.Request.Visit(e.Attr("href"))
+
+		//bodyData := string(r.Body)
+		//dom, _ := goquery.NewDocumentFromReader(strings.NewReader(bodyData))
+
+		fmt.Println("OnResponse收到html内容后调用:OnHTML ")
+
+		link := e.Attr("href")
+		link = e.Request.AbsoluteURL(link)
+
+		fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+		fmt.Println("")
+		//c.Visit(e.Request.AbsoluteURL(link))
+	})
+
+	c.OnResponse(func(r *colly.Response) {
+		fmt.Println("获得响应后调用:OnResponse")
+		//fmt.Println("Visited", r.Request.URL)
 	})
 
 	c.OnRequest(func(r *colly.Request) {
