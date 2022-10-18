cd5ff4dbe12adfb561377e3d0d8700513e6ec7ae
wangshuo <xujieiata@163.com>
2022-10-18 11:34:26 +0800

    例子4

--- spider/src/crawl/teamCrawl.go
+++ spider/src/crawl/teamCrawl.go
@@ -1,1 +1,36 @@
 package crawl
+
+import (
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"github.com/gocolly/colly"
+	"spider/src/config"
+	"spider/src/utils"
+	"strings"
+)
+
+// 爬取战队的网页数据
+func CrawlTeam(matchUrl string) {
+	DB := config.GetDB() // 初始化数据库句柄
+
+	c := colly.NewCollector(
+		// 允许重复访问
+		colly.AllowURLRevisit())
+
+	c.OnRequest(func(r *colly.Request) {
+		r.Headers.Set("User-Agent", utils.RandomString())
+	})
+
+	c.OnHTML("div.contentCol", func(e *colly.HTMLElement) {
+		content, _ := e.DOM.Html()
+		dom, _ := goquery.NewDocumentFromReader(strings.NewReader(content))
+		fmt.Println(DB, dom)
+
+	})
+
+	c.OnResponse(func(r *colly.Response) {
+		fmt.Println("Visited ", r.Request.URL.String())
+	})
+
+	c.Visit(matchUrl)
+}
--- spider/src/crawl/teamParsing.go
+++ spider/src/crawl/teamParsing.go
@@ -1,1 +1,10 @@
 package crawl
+
+import (
+	"github.com/PuerkitoBio/goquery"
+)
+
+func ParseMatchTeam(dom *goquery.Document) {
+	//解析比赛战队网页
+
+}
